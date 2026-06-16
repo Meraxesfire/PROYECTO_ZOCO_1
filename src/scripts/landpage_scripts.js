d84cc6a5c56c0608eae94d6dd69f0aec7bf80021
Meraxesfire <isabelcaromorillo@gmail.com>
2026-06-16 12:00:54 +0200

    Added: divSupremo, to wrap the first big div and improve the scroll system. Added separated stylesheet. Added: separated JS page for scroll system on landpage. Modified: Div positions on css to improve scroll system and make it independent from the added media, always in minf that first saw is a video/img/post that occupies the whole screen until user scrolls, in order to change it trough marketing seasons

--- src/scripts/landpage_scripts.js
+++ src/scripts/landpage_scripts.js
@@ -1,2 +1,21 @@
 
 //script de landpage
+const header = document.querySelector('.divSuperior');
+const triggerElement = document.querySelector('.divSupremo'); // Usamos el contenedor padre
+
+const observerOptions = {
+    root: null,
+    threshold: 0.1 // Cuando el 10% del contenedor padre ha salido del viewport
+};
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+        if (!entry.isIntersecting) {
+            header.classList.add('scrolled'); // Aquí cambias el color/opacidad
+        } else {
+            header.classList.remove('scrolled');
+        }
+    });
+}, observerOptions);
+
+observer.observe(triggerElement);
